db9744c9bee23dabc7c612583881a005fc020f28
Dipesh Pardeshi <152983518+dipeshpardeshi04@users.noreply.github.com>
2024-06-23 15:09:49 +0530

    Update pagecards.js

--- src/pagecards.js
+++ src/pagecards.js
@@ -1,23 +1,32 @@
-import { useState } from "react";
+import  { useState } from "react";
+import PropTypes from "prop-types";
 
-function PageCard({title, desc, buttonText, theme, icon}) {
+function PageCard({ title, desc, buttonText, theme, icon }) {
     const [isHovered, setIsHovered] = useState(false);
-  return (
-    <div 
-        className="page-card rounded d-flex flex-column justify-content-start align-items-center align-items-md-start"
-        style={{
-            border: `1px solid ${theme}`,
-            backgroundColor: `${isHovered?theme:'white'}`,
-            color: `${!isHovered?'black':'white'}`,
-        }}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-    >
-        <h4 className="d-flex align-items-center gap-3 f">{icon}<span>{title}</span></h4>
-        <p className="desc">{desc}</p>
-        <button type="button" className="btn">{buttonText}</button>
-    </div>
-  )
+    return (
+        <div 
+            className="page-card rounded d-flex flex-column justify-content-start align-items-center align-items-md-start"
+            style={{
+                border: `1px solid ${theme}`,
+                backgroundColor: `${isHovered ? theme : 'white'}`,
+                color: `${!isHovered ? 'black' : 'white'}`,
+            }}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <h4 className="d-flex align-items-center gap-3 f">{icon}<span>{title}</span></h4>
+            <p className="desc">{desc}</p>
+            <button type="button" className="btn">{buttonText}</button>
+        </div>
+    );
 }
 
-export default PageCard
+PageCard.propTypes = {
+    title: PropTypes.string.isRequired,
+    desc: PropTypes.string.isRequired,
+    buttonText: PropTypes.string.isRequired,
+    theme: PropTypes.string.isRequired,
+    icon: PropTypes.element.isRequired,
+};
+
+export default PageCard;
